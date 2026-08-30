eb1d8413359f2288ccf83f36883a02d8ac7afb01
Coder-blessed <164379434+coder-blessed@users.noreply.github.com>
2026-08-30 21:20:34 +0100

    Scores
    Update

--- lib/content.ts
+++ lib/content.ts
@@ -658,11 +658,11 @@ export const matches: Record<TeamKey, TeamMatches> = {
       date: "Sun 30 Aug 2026",
       venue: "Samuel Ogbemudia Stadium, Benin City",
       home: {
-        name: "Bendel Insurance",
+        name: "Bendel Insurance  2",
         tone: 0,
       },
       away: {
-        name: "Warri Wolves",
+        name: "Warri Wolves.  1",
         tone: 1,
       },
       kickoff: "TBA",
@@ -835,13 +835,13 @@ export const seasonFixtures: Fixture[] = [
     home: {
       name: "Bendel Insurance",
       tone: 0,
-      shortName: "BEN",
+      shortName: "BEN 2",
       logo: TEAM_LOGOS["Bendel Insurance"],
     },
     away: {
       name: "Warri Wolves",
       tone: 1,
-      shortName: "WAR",
+      shortName: "WAR. 1",
       logo: TEAM_LOGOS["Warri Wolves"],
     },
     kickoff: "16:00 WAT",
